380face61ca5cb3627521aabb7c731c92b0a925c
DriveFleet Developer <dev@drivefleet.com>
2026-05-21 15:33:55 +0600

    Fix: Remove manual DNS override blocking MongoDB connection on Vercel

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
-
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
